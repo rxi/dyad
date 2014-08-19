33e5c34a1a365bbd79dc9868a9125a2154bbdef8
rxi <rxi@users.noreply.github.com>
2014-08-19 21:11:32 +0100

    Fixed handling of NULL return from realloc() in dyad_realloc()

--- src/dyad.c
+++ src/dyad.c
@@ -75,7 +75,7 @@ static void dyad_panic(const char *fmt, ...);
 
 static void *dyad_realloc(void *ptr, int n) {
   ptr = realloc(ptr, n);
-  if (!ptr) {
+  if (!ptr && n != 0) {
     dyad_panic("out of memory");
   }
   return ptr;

3fed186e37cd355d0f597d04bf21ab8fb4633088
rxi <rxi@users.noreply.github.com>
2014-09-05 19:06:27 +0100

    Added daytime.c example

---
+++ example/daytime.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "dyad.h"
+
+/* Conneccts to a daytime server and prints the response */
+
+static void onConnect(dyad_Event *e) {
+  printf("connected: %s\n", e->msg);
+}
+
+static void onError(dyad_Event *e) {
+  printf("error: %s\n", e->msg);
+}
+
+static void onData(dyad_Event *e) {
+  printf("%s", e->data);
+}
+
+
+int main(void) {
+  dyad_Stream *s;
+  dyad_init();
+
+  s = dyad_newStream();
+  dyad_addListener(s, DYAD_EVENT_CONNECT, onConnect, NULL);
+  dyad_addListener(s, DYAD_EVENT_ERROR,   onError,   NULL);
+  dyad_addListener(s, DYAD_EVENT_DATA,    onData,    NULL);
+  dyad_connect(s, "wolfnisttime.com", 13);
+
+  while (dyad_getStreamCount() > 0) {
+    dyad_update();
+  }
+  
+  dyad_shutdown();
+  return 0;
+}

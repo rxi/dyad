f11873bac881134e8c3ec34e75bdee1e70dc87f4
rxi <rxi@users.noreply.github.com>
2014-09-06 18:03:31 +0100

    Changed httpserv.c example to be less similar to httpserv2.c

--- example/httpserv.c
+++ example/httpserv.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "dyad.h"
 
-/* A very simple HTTP server. See httpserv2.c for an example which uses
- * DYAD_EVENT_READY and udata to send the requested file in chunks rather than
- * loading the entire file into a stream's write buffer. */
- 
+/* A very simple HTTP server which responds to a number of paths with different
+ * information. See httpserv2.c for an example of an HTTP server which responds
+ * with files. */
+
+static int count = 0;
 
 static void onLine(dyad_Event *e) {
   char path[128];
@@ -15,19 +17,26 @@ static void onLine(dyad_Event *e) {
     printf("%s %s\n", dyad_getAddress(e->stream), path);
     /* Send header */
     dyad_writef(e->stream, "HTTP/1.1 200 OK\r\n\r\n");
-    /* Check request */
-    if (strstr(path, "..") || path[1] == '/') {
-      /* Handle bad request */
-      dyad_writef(e->stream, "bad request '%s'", path);
+    /* Handle request */
+    if (!strcmp(path, "/")) {
+      dyad_writef(e->stream, "<html><body><pre>"
+                             "<a href='/date'>date</a><br>"
+                             "<a href='/count'>count</a><br>"
+                             "<a href='/ip'>ip</a>"
+                             "</pre></html></body>" );
+
+    } else if (!strcmp(path, "/date")) {
+      time_t t = time(0);
+      dyad_writef(e->stream, "%s", ctime(&t));
+
+    } else if (!strcmp(path, "/count")) {
+      dyad_writef(e->stream, "%d", ++count);
+
+    } else if (!strcmp(path, "/ip")) {
+      dyad_writef(e->stream, "%s", dyad_getAddress(e->stream));
+
     } else {
-      /* Handle good request */
-      FILE *fp = fopen(path + 1, "rb");
-      if (fp) {
-        dyad_writef(e->stream, "%r", fp);
-        fclose(fp);
-      } else {
-        dyad_writef(e->stream, "not found '%s'\n", path);
-      }
+      dyad_writef(e->stream, "bad request '%s'", path);
     }
     /* Close stream when all data has been sent */
     dyad_end(e->stream);
@@ -39,7 +48,7 @@ static void onAccept(dyad_Event *e) {
 }
 
 static void onListen(dyad_Event *e) {
-  printf("server listening on port %d\n", dyad_getPort(e->stream));
+  printf("server listening: http://localhost:%d\n", dyad_getPort(e->stream));
 }
 
 static void onError(dyad_Event *e) {
